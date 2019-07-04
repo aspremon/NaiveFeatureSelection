12eb59876018e97958aca808b7694375588e8f38
Loïc Estève <loic.esteve@ymail.com>
2019-07-04 15:45:15 +0200

    Fix setup.py.

--- setup.py
+++ setup.py
@@ -1,40 +1,20 @@
 from setuptools import find_packages, setup
 
-DEPENDENCY_PACKAGE_NAMES = ["numpy","sklearn",]
-
-
-def check_dependencies():
-    missing_dependencies = []
-    for package_name in DEPENDENCY_PACKAGE_NAMES:
-        try:
-            __import__(package_name)
-        except ImportError:
-            missing_dependencies.append(package_name)
-
-    if missing_dependencies:
-        raise ValueError(
-            'Missing dependencies: {}. We recommend you follow '
-            'the installation instructions at '
-            'https://github.com/hassony2/manopth#installation'.format(
-                missing_dependencies))
-
-
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-check_dependencies()
-
 setup(
-    name="NaiveFeatureSelection",
+    name="naive_feature_selection",
     version="0.0.1",
     author="Armin Askari, Alexandre d'Aspremont, Laurent El Ghaoui",
     author_email="aspremon@ens.fr",
-    packages=find_packages(exclude=('tests',)),
+    packages=find_packages(),
     python_requires=">=3.5.0",
+    install_requires=["numpy"],
     description="Naive Feature Selection",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/aspremon/NFS",
+    url="https://github.com/aspremon/NaiveFeatureSelection",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT",
