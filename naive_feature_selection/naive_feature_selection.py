c8f8ecf6a52606ac3afe15b3a75dfb57b091349a
qrebjock <quentin.rebjock@gmail.com>
2019-11-07 14:42:17 +0100

    Separating SNB and the NFS estimator

--- naive_feature_selection/naive_feature_selection.py
+++ naive_feature_selection/naive_feature_selection.py
@@ -1,13 +1,13 @@
 import numpy as np
+
+from typing import Union
+
 from sklearn.base import BaseEstimator
 from sklearn.feature_selection.base import SelectorMixin
 from sklearn.utils import check_X_y
 from sklearn.utils.validation import check_is_fitted
 
-
-######################################################################
-# Base classes
-###################################################################### 
+from naive_feature_selection.sparse_naive_bayes import sparse_naive_bayes
 
 
 class NaiveFeatureSelection(BaseEstimator, SelectorMixin):
@@ -18,169 +18,15 @@ class NaiveFeatureSelection(BaseEstimator, SelectorMixin):
     alpha : typo robustness parameter (same alpha as in MNB from sklearn)
     """
 
-    def __init__(self, k, alpha=1e-10):
+    def __init__(self, k: Union[int, str], alpha: float = 1e-10):
         self.k = k
         self.alpha = alpha
-
-    def _split_classes(self, X, y, label=0): # TODO: Use scikit learn here
-        """
-        split_classes function
-
-        Description: Function that takes the original
-        data and partitions it into two classes.
-
-        Arguments:
-            - X: training data as a n x m matrix (n datapoints
-            with m features per data point)
-            - Y: class labels for the n different points
-            - label: desired class
-
-        Returns:
-            - x1: all the data for class = label
-            - x2: the data for class != label
-
-        Note: this only works for binary classification
-        """
-        idx1 = np.where(y == label)[0]
-        idx2 = np.where(y != label)[0]
-        return {"class1": X[idx1, :], "class2": X[idx2, :]}
-
-    def _class_prob(self, y, label=0): # TODO: Use scikit learn here
-        """
-        class_prob function
-
-        Description: Function that finds the ratio
-        of class1 and class2 labels to use as the
-        prior probability for naive bayes classification.
-
-        Arguments:
-            - Y: class labels for the n different points
-            - label: desired class
-        
-        Returns:
-            - pc1: probability of class = label
-            - pc2: probability of class != label
-        """
-        n = len(y)
-        idx = np.where(y == label)[0]
-        return {"pc1": len(idx) * 1.0 / n, "pc2": (n - len(idx)) * 1.0 / n}
-
-    def _entr(self, x):
-        """
-        Negative entropy function.
-
-        This returns sum_i x_i * log(x_i) 
-        """
-        res = np.zeros(x.shape)
-        res[np.nonzero(x)] = x[np.nonzero(x)] * np.log(x[np.nonzero(x)])
-        return res
-
-    def _fun(self, a, k, c, f1, f2):
-        """
-        Auxialiary gradient function.
-        """
-        h = c - f1 * np.log(a) - f2 * np.log(1 - a)
-        idx = np.argpartition(h, -k)[-k:]
-        # fval = sum(h[idx])
-        nabla_a = -f1 / a + f2 / (1 - a)
-        df = np.sum(nabla_a[idx])
-        return df
-
-
-    def _naive_feature_selection(self, X, y, k):
-        """
-        Naive Feature Selection
-
-        Description: Function that solves the following multinomial naive Bayes
-        training problem, written
-
-        max_{q,r}   fp^T log q + fm^T log r
-
-            s.t. 	\|q - r\|_0 <= k
-                    0 <= q, r
-                    sum(q) = 1, sum(r) = 1
-
-        Arguments:
-            - X: training data as a n x m matrix (n datapoints
-            with m features per data point)
-            - Y: binary class labels for the n different points (one of the labels should be 1)
-            - k: target cardinality for vector in linear classification rule
-
-        Returns:
-            - idx: 	Index of selected features
-            - q,r: 	Solutions reconstructed from optimal dual solution
-            - w0,w: linear classification rule: w0 + w'*x > 0, where w is k sparse.
-        """
-
-        # First construct fp, fm, by summing feature vectors for each class
-        split = self._split_classes(X, y, label=1)
-
-        C1 = split['class1'].shape[0]
-        C2 = split['class2'].shape[0]
-
-        f1 = np.sum(split["class1"], axis=0)
-        f2 = np.sum(split["class2"], axis=0)
-        f1 = np.squeeze(np.asarray(f1))
-        f2 = np.squeeze(np.asarray(f2))
-
-        f1 += self.alpha*C1*np.ones(f1.shape)
-        f2 += self.alpha*C2*np.ones(f2.shape)
-
-
-
-        # Define dual objective function
-        alpha_low = 0
-        alpha_top = 1
-        c = self._entr(f1) + self._entr(f2) - self._entr(f1 + f2)
-        # Solve dual problem by bisection
-        tol = 1e-6
-        while (alpha_top - alpha_low) > 1e-10:
-            alpha = (alpha_top + alpha_low) / 2.0
-            df = self._fun(alpha, k, c, f1, f2)
-            # print(df)
-            if df > tol:
-                alpha_top = alpha
-            elif df < -tol:  # TODO: check if OK stopping on gradient?
-                alpha_low = alpha
-            else:
-                break
-
-        # Get primal points from dual solution
-        h = c - f1 * np.log(alpha) - f2 * np.log(1 - alpha)
-        idx = np.argpartition(h, -k)[-k:]
-        mask = np.ones(len(f1), dtype=bool)  # all elements included/True.
-        mask[idx] = False
-        qopt = np.zeros(len(f1))
-        ropt = np.zeros(len(f2))
-        qopt[mask] = (f1[mask] + f2[mask]) / sum(f1 + f2)
-        ropt[mask] = qopt[mask]
-        qopt[idx] = sum(f1[idx] + f2[idx]) / (sum(f1[idx]) * sum(f1 + f2)) * f1[idx]
-        ropt[idx] = sum(f1[idx] + f2[idx]) / (sum(f2[idx]) * sum(f1 + f2)) * f2[idx]
-        objv = f1[np.nonzero(f1)].dot(np.log(qopt[np.nonzero(f1)])) + f2[
-            np.nonzero(f2)
-        ].dot(np.log(ropt[np.nonzero(f2)]))
-
-        # Get linear classification rule: w0 + w'*x > 0
-        pc = self._class_prob(y, label=1)
-        w0 = np.log(pc["pc1"]) - np.log(pc["pc2"])
-        with np.errstate(divide="ignore"):
-            w1 = np.log(qopt)
-            idx_inf = np.isinf(w1)
-            w1[idx_inf] = -33.6648265  # essentially the 0 entries, log(0) to -33.6648
-            w2 = np.log(ropt)
-            idx_inf = np.isinf(w2)
-            w2[idx_inf] = -33.6648265
-        w = w1 - w2
-
-        # Return results
-        return {"idx": idx, "w0": w0, "w": w, "q": qopt, "r": ropt, "objv": objv}
-
 
     def _check_params(self, X, y):
         if not (self.k == "all" or 0 <= self.k <= X.shape[1]):
             raise ValueError(
-                "k should be >=0, <= n_features = %d; got %r. "
-                "Use k='all' to return all features." % (X.shape[1], self.k)
+                f"k should be >= 0, <= n_features = {X.shape[1]}; "
+                f"got {self.k}. Use k='all' to return all features."
             )
 
     def fit(self, X, y):
@@ -193,7 +39,7 @@ class NaiveFeatureSelection(BaseEstimator, SelectorMixin):
         y : array-like, shape = [n_samples]
             The target values (class labels in classification, real numbers in
             regression).
-        k : target number of variables
+        (k : target number of variables)
         Returns
         -------
         self : object
@@ -207,15 +53,13 @@ class NaiveFeatureSelection(BaseEstimator, SelectorMixin):
         elif self.k == 0:
             mask = np.zeros(X.shape[1], dtype=bool)
         else:
-            res_nfs = self._naive_feature_selection(X, y, self.k)
+            res_nfs = sparse_naive_bayes(X, y, self.k, alpha=self.alpha)
             mask = np.zeros(X.shape[1], dtype=bool)
             mask[res_nfs["idx"]] = 1
 
         self.mask_ = mask
         return self
 
-
     def _get_support_mask(self):
         check_is_fitted(self, "mask_")
         return self.mask_
-
---
+++ naive_feature_selection/sparse_naive_bayes.py
@@ -0,0 +1,228 @@
+import numpy as np
+
+
+_log_0 = -33.6648265
+
+
+def _split_classes(x, y, label=0):  # TODO: Use scikit learn here
+    """
+    split_classes function
+
+    Description: Function that takes the original
+    data and partitions it into two classes.
+
+    Arguments:
+        - X: training data as a n x m matrix (n datapoints
+        with m features per data point)
+        - Y: class labels for the n different points
+        - label: desired class
+
+    Returns:
+        - x1: all the data for class = label
+        - x2: the data for class != label
+
+    Note: this only works for binary classification
+    """
+    return x[np.where(y == label)[0], :], x[np.where(y != label)[0], :]
+
+
+def _class_prob(y, label=0):  # TODO: Use scikit-learn here
+    """
+    class_prob function
+
+    Description: Function that finds the ratio
+    of class1 and class2 labels to use as the
+    prior probability for naive bayes classification.
+
+    Arguments:
+        - Y: class labels for the n different points
+        - label: desired class
+
+    Returns:
+        - pc1: probability of class = label
+        - pc2: probability of class != label
+    """
+    s = np.sum(y == label)
+    return s / len(y), 1 - s / len(y)
+
+
+def _largest_indices(ar, k):
+    """
+    Computes the k largest indices of a given array.
+    :param ar: Array whose largest indices have to be computed
+    :param k: Number of indices to compute
+    :return: The k largest indices of ar
+    """
+    return np.argpartition(ar, -k)[-k:]
+
+
+def negative_entropy(x):
+    """
+    Negative entropy function.
+
+    This returns sum_i x_i * log(x_i) where log(x) = 0 if x < 0.
+    """
+    res = np.zeros(x.shape)
+    ind = x > 0
+    res[ind] = x[ind] * np.log(x[ind])
+    return res
+
+
+def auxiliary_gradient(alpha, k, c, f1, f2):
+    """
+    Auxiliary gradient function.
+    """
+    h = c - f1 * np.log(alpha) - f2 * np.log(1 - alpha)
+    idx = _largest_indices(h, k)
+    # fval = np.sum(h[idx])
+    grad_h = -f1 / alpha + f2 / (1 - alpha)
+    grad_phi = np.sum(grad_h[idx])
+    return grad_phi, idx
+
+
+def bisect_phi(f1, f2, k, tol: float, alpha_stop: float, f=None, c=None):
+    """
+    Dual objective function to be optimized. Bisection method is used.
+    """
+    if f is None:
+        f = f1 + f2
+    if c is None:
+        c = negative_entropy(f1) + negative_entropy(f2) - negative_entropy(f)
+    alpha_low, alpha_top = 0, 1
+    while True:
+        alpha = (alpha_top + alpha_low) / 2.0
+        grad_phi, idx = auxiliary_gradient(alpha, k, c, f1, f2)
+        if (alpha_top - alpha_low) <= alpha_stop:
+            break
+        if grad_phi > tol:
+            alpha_top = alpha
+        elif grad_phi < -tol:  # TODO: check if OK stopping on gradient?
+            alpha_low = alpha
+        else:
+            break
+    print(alpha)
+    return idx
+
+
+def reconstruct_primal_point(f1, f2, idx, f_sum=None):
+    """
+    Reconstruct a primal feasible (sub-optimal) point.
+    :param f1: Naive data average (positive class)
+    :param f2: Naive data average (negative class)
+    :param idx: Set of indices of the top k entries of h(alpha*)
+    :param f_sum: sum(f1 + f2) Can be passed if pre-computed
+    :return:
+    """
+    if f_sum is None:
+        f_sum = np.sum(f1, + f2)
+
+    mask = np.ones(len(f1), dtype=bool)  # all elements included/True.
+    mask[idx] = False
+
+    q_opt = np.zeros(len(f1))
+    r_opt = np.zeros(len(f2))
+
+    q_opt[mask] = (f1[mask] + f2[mask]) / f_sum
+    r_opt[mask] = q_opt[mask]
+
+    q_opt[idx] = np.sum(f1[idx] + f2[idx]) * f1[idx] / (np.sum(f1[idx]) * f_sum)
+    r_opt[idx] = np.sum(f1[idx] + f2[idx]) * f2[idx] / (np.sum(f2[idx]) * f_sum)
+
+    return q_opt, r_opt
+
+
+def find_linear_classification_rule(q_opt, r_opt, pc1, pc2):
+    """
+    Finds the linear classification rule w_0 + w' * w > 0
+    :return:
+    """
+    w_0 = np.log(pc1) - np.log(pc2)
+    with np.errstate(divide='ignore'):
+        w1 = np.log(q_opt)
+        idx_inf = np.isinf(w1)
+        w1[idx_inf] = _log_0  # essentially the 0 entries, log(0) to -33.6648
+        w2 = np.log(r_opt)
+        idx_inf = np.isinf(w2)
+        w2[idx_inf] = _log_0
+    w = w1 - w2
+    return w_0, w
+
+
+def sparse_naive_bayes_from_averages(
+        f1: np.ndarray, f2: np.ndarray, pc1: float, pc2: float, k: int,
+        tol: float = 1e-6, alpha_stop: float = 1e-10, f=None, f_sum=None, c=None,
+):
+    if f is None:
+        f = f1 + f2
+    if f_sum is None:
+        f_sum = np.sum(f)
+
+    idx = bisect_phi(f1, f2, k, tol, alpha_stop, f=f, c=c)
+
+    q_opt, r_opt = reconstruct_primal_point(f1, f2, idx, f_sum=f_sum)
+
+    objv = f1[f1 != 0] @ np.log(q_opt[f1 != 0]) + f2[f2 != 0] @ np.log(r_opt[f2 != 0])
+
+    w_0, w = find_linear_classification_rule(q_opt, r_opt, pc1, pc2)
+
+    return {
+        'idx': idx,
+        'q': q_opt,
+        'r': r_opt,
+        'objv': objv,
+        'w0': w_0,
+        'w': w
+    }
+
+
+def naive_bayes_data_averages(x, y, alpha: float = 1e-10):
+    """
+    Computes the required data averages to run sparse naive bayes.
+    :param x: Feature matrix
+    :param y: Label vector
+    :param alpha: Laplace smoothing parameter
+    :return: (f1, f2, pc1, pc2) where
+        - f1 is the sum of the lines of x with positive label
+        - f2 is the sum of the lines of x with negative label
+        - pc1 is an estimation of the probability of the positive class
+        - pc2 is an estimation of the probability of the negative class
+    """
+    split1, split2 = _split_classes(x, y, label=1)
+    c1, c2 = split1.shape[0], split2.shape[0]
+
+    f1 = np.squeeze(np.sum(split1, axis=0)) + alpha * c1
+    f2 = np.squeeze(np.sum(split2, axis=0)) + alpha * c2
+
+    pc1, pc2 = _class_prob(y, label=1)
+
+    return f1, f2, pc1, pc2
+
+
+def sparse_naive_bayes(x: np.ndarray, y: np.ndarray, k: int, alpha: float = 1e-10, **kwargs):
+    """
+    Sparse Naive Bayes
+
+    Description: Function that solves the following multinomial naive Bayes
+    training problem, written
+
+    max_{q,r}   fp^T log q + fm^T log r
+
+        s.t. 	||q - r||_0 <= k
+                0 <= q, r
+                sum(q) = 1, sum(r) = 1
+
+    Arguments:
+        - X: training data as a n x m matrix (n data points with m features per data point)
+        - Y: binary class labels for the n different points (one of the labels should be 1)
+        - k: target cardinality for vector in linear classification rule
+        - alpha: Laplacian smoothing parameter
+
+    Returns:
+        - idx: Index of selected features
+        - q, r: Solutions reconstructed from optimal dual solution
+        - w0, w: Linear classification rule: w0 + w' * x > 0, where w is k sparse.
+    """
+
+    f1, f2, pc1, pc2, = naive_bayes_data_averages(x, y, alpha=alpha)
+
+    return sparse_naive_bayes_from_averages(f1, f2, pc1, pc2, k, **kwargs)
